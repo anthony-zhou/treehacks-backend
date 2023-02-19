103cc95a4b4fd78a7e99a4c6d5a36fb2ee83722a
Zac Schulwolf <zacwolf3@gmail.com>
2023-02-19 04:06:53 +0000

    Chunk to gpt

---
+++ chunkify.py
@@ -0,0 +1,97 @@
+
+
+def clean_json(data):
+    new_data = []
+    last_speaker = None
+    for d in data:
+        d["text"] = d["text"].strip()
+        if len(d["text"]) == 0:
+            continue
+        if d["speaker_id"] == last_speaker:
+            d_prev = new_data[-1]
+            d_prev["text"] = d_prev["text"] + " " + d["text"]
+            d_prev["time_end"] = d["time_end"]
+            d_prev["num_words"] += d["num_words"]
+            new_data[-1] = d_prev
+        else:
+            new_data.append(d)
+        last_speaker = d["speaker_id"]
+    return new_data
+
+def json_to_transcript(data):
+    transcript = ""
+    i = 0 
+    last_speaker = None
+    for d in data:
+        if len(d["text"]) == 0:
+            continue
+        if d["speaker_id"] == last_speaker:
+            transcript += " " + d["text"].strip()
+        else:
+            # transcript += "\n\n" + d["speaker_id"] + ":\n" + d["text"].strip()
+            transcript += "\n\n" + d["role"] + ":\n" + d["text"].strip()
+            i+=1
+        last_speaker = d["speaker_id"]
+    print(i)
+    return transcript
+
+def create_chunk(data, chunk_size="small", end_index = None):
+    """Create chunks of conversation exchanges based on chunk_size from the end of the data."""
+    if end_index is None:
+        end_index = len(data)
+
+    if chunk_size == "small":
+        # 1 interviewer followed by 1 interviewee potentially followed by 1 interviewer
+        # or enough for 300 words
+        interviewee_speak = 0
+        interviewer_speak = 0
+        chunk_start = 0
+        num_words = 0
+        success = False
+        for i, d in enumerate(reversed(data[:end_index])):
+            if d["role"] == "INTERVIEWEE":
+                interviewee_speak += 1
+            else:
+                interviewer_speak += 1
+
+            num_words += d["num_words"]
+
+            if interviewee_speak >= 1 and interviewer_speak >= 1 and num_words >= 300:
+                # End on with this being the last paragraph
+                chunk_start = end_index - i - 1
+                success = True
+                break
+        
+        return success, chunk_start, end_index
+
+def chunkify(data, chunk_size, max_chunks = None):
+    chunks_idxs = []
+
+    success = True
+    next_chunk_end = len(data)
+    while success:
+        success, chunk_start, chunk_end = create_chunk(data, chunk_size, end_index = next_chunk_end)
+        if success:
+            chunks_idxs.append((chunk_start, chunk_end))
+            next_chunk_end = chunk_start-1
+        else:
+            # If we can't find a chunk, then we just expand the most recently calculated chunk to the beginning of the data
+            chunks_idxs[-1] = (0, chunks_idxs[-1][1])
+            success = False
+
+    if max_chunks is not None:
+        chunks_idxs = chunks_idxs[-max_chunks:]
+    return chunks_idxs
+
+
+# chunk_segments = chunkify(data_clean, "small")
+# [json_to_transcript(data_clean[s:e]) for s,e in chunk_segments]
+
+
+
+# if main
+if __name__ == "main":
+    import json
+    file_name = "lauren_brooks"
+    with open(f"data/{file_name}.json") as f:
+        data = json.load(f)
---
+++ part1.py
@@ -0,0 +1,57 @@
+import json
+import openai
+import dotenv
+import os
+import re
+
+from chunkify import clean_json, chunkify, json_to_transcript
+dotenv.load_dotenv()
+
+OPENAI_KEY1 = os.environ.get("OPENAI_KEY1")
+
+openai.api_key = OPENAI_KEY1
+
+PROMPT = "I am going to share a chunk of a user interview. This is an exploratory interview where our general learning objective is to learn more about how the interviewee uses talent sharing. Could you please come up with a formal, numbered, list of follow up questions that we can ask the interviewer relating to what we are talking about at the end?  Please make the question be a natural response directly related to what the interviewee just said.\nHere is the interview:"
+
+# Function that given a recording json, return a either a list of questions
+def get_questions(file_name="lauren_brooks"):
+    with open(f"data/{file_name}.json") as f:
+        data = json.load(f)
+    
+
+    data_clean = clean_json(data)
+    chunks = chunkify(data_clean, "small", 1)
+    assert len(chunks) == 1
+    chunk_start, chunk_end = chunks[0]
+    chunk = data_clean[chunk_start:chunk_end]
+    transcript = json_to_transcript(chunk)
+    print(transcript)
+
+    gpt_prompt = PROMPT + "\n\n" + transcript.strip()
+
+
+    completion = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=gpt_prompt,
+        temperature=.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    gpt_output = completion.choices[0].text.strip()
+    print(gpt_output)
+
+    # We will have to modify regex for other output types
+
+    # \n1. ... \n2. ... \n3. ...
+    pattern_number = r"\d\..+\n"
+
+    questions = [x.strip() for x in re.findall(pattern_number, gpt_output)]
+
+    return questions
+
+
+
+
+get_questions()
